da13e7139e22d5bca7ceca626de764647ca9b882
rayne <pothong.r@gmail.com>
2025-09-28 19:15:15 -0700

    added second task

--- array-methods.js
+++ array-methods.js
@@ -15,9 +15,9 @@
     let newString = str.split('-')
     newString = newString.map(
     function(word, index){
-       if (index ==0){
-         return word.toLowerCase()
-       } else {
+       if (index === 0){
+        return word.toLowerCase()
+       }else{
         return word[0].toUpperCase() + word.slice(1).toLowerCase()
        }
      }
@@ -30,3 +30,24 @@ console.log(camelize("This-Word"))
 console.log(camelize("This-IS-A-longer-String"))
 // ------------------------------------------------------
 
+// 2. Filter range
+// importance: 4
+// Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
+
+// The function should not modify the array. It should return the new array.
+
+// For instance:
+
+let arr = [5, 3, 8, 1];
+
+function filterRange(arr, a, b){
+    return arr.filter(item => (a <= item && item <= b))
+}
+
+let filtered = filterRange(arr, 1, 4)
+
+console.log( filtered ); // 3,1 (matching values)
+
+console.log( arr ); // 5,3,8,1 (not modified)
+
+// -------------------------------------------------------------------------------
