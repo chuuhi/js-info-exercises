6b209d643a3effe7e8924d0850a0df21cd2ecdca
rayne <pothong.r@gmail.com>
2025-09-30 13:50:20 -0700

    added task 9

--- array-methods.js
+++ array-methods.js
@@ -206,3 +206,27 @@ let usersMapped = users.map (user => ({
 // console.log( usersMapped[0].fullName ) // John Smith
 
 // -------------------------------------------------------------------------------
+// 9. Sort users by age
+// importance: 5
+// Write the function sortByAge(users) that gets an array of objects with the age property and sorts them by age.
+
+// For instance:
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [ pete, john, mary ];
+
+function sortByAge(arr){
+  arr.sort((a,b) => a.age - b.age)
+}
+
+// sortByAge(arr);
+
+// // // now: [john, mary, pete]
+// console.log(arr[0].name); // John
+// console.log(arr[1].name); // Mary
+// console.log(arr[2].name); // Pete
+
+// --------------------------------------------------------------------------------
