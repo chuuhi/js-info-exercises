b59aa22fc40e5d1015a4cd72196f2d7a7a0dd212
rayne <pothong.r@gmail.com>
2025-09-30 14:02:32 -0700

    added task 10

--- array-methods.js
+++ array-methods.js
@@ -230,3 +230,30 @@ function sortByAge(arr){
 // console.log(arr[2].name); // Pete
 
 // --------------------------------------------------------------------------------
+
+// 10. Shuffle an array
+// importance: 3
+// Write the function shuffle(array) that shuffles (randomly reorders) elements of the array.
+
+// Multiple runs of shuffle may lead to different orders of elements. For instance:
+
+// let arr = [1, 2, 3];
+
+// shuffle(arr);
+// // arr = [3, 2, 1]
+
+// shuffle(arr);
+// // arr = [2, 1, 3]
+
+// shuffle(arr);
+// // arr = [3, 1, 2]
+// // ...
+
+function shuffle(array){
+  for (let i = array.length - 1; i > 0; i--){
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], [array[i]]];
+  }
+}
+
+// ---------------------------------------------------------------------------------
