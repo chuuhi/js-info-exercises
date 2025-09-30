a9f13bbe357d8d066925afedf4bc32ebde722507
rayne <pothong.r@gmail.com>
2025-09-30 14:47:19 -0700

    added task 12

--- array-methods.js
+++ array-methods.js
@@ -281,3 +281,28 @@ function getAverageAge(users){
 console.log( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
 
 // ---------------------------------------------------------------------------------
+
+// 12. Filter unique array members
+// importance: 4
+// Let arr be an array.
+
+// Create a function unique(arr) that should return an array with unique items of arr.
+
+// For instance:
+
+function unique(arr){
+  let result = []
+
+  for (let str of arr){
+    if (!result.includes(str)){
+      result.push(str)
+    }
+  }
+  return result
+}
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O" ];
+  
+console.log( unique(strings) ); // Hare, Krishna, :-O
+  
+  // -------------------------------------------------------------------------------
